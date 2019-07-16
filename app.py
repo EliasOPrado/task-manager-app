0440f0aea882bf740def11c77f1d2778e2394c6f
Your NameEliasOPrado <eliaspradoprofessional@outlook.com>
2019-07-16 20:43:06 +0000

    modified app.py and templates

--- app.py
+++ app.py
@@ -13,12 +13,14 @@ app.config['MONGO_URI'] = 'mongodb+srv://elias:kb01210012@myfirstcluster-uyvei.m
 
 mongo = PyMongo(app)
 
-
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+@app.route('/add_task')
+def add_task():
+    return render_template("addtask.html", categories=mongo.db.tasks.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
